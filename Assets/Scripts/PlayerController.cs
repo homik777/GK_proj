1781c983a2d2670c54c1acaeee29d0d80efd9dd9
Rafał Potoczek <rafal.potoczek@gmail.com>
2016-04-03 18:12:30 +0200

    Dodałem model helikoptera i kręcenie się śmigieł, pochylanie do przodu i do tyłu też

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,7 @@ public class Boundary
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-    public const float maxSpeed=2;
-    public float defaultHigh = 1.72f;
+    public float defaultHigh;
     public float tilt;
     public Boundary boundary;
 
@@ -19,15 +18,13 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        speed = 5;
 	    rb = GetComponent<Rigidbody>();
 	}
-
+    
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(rightLeft(), 0.0f, upDown());
-
 
         rb.velocity = movement * speed;
 
@@ -35,7 +32,7 @@ public class PlayerController : MonoBehaviour {
             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
             defaultHigh,
             Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
-        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt  +90);
+        rb.rotation = Quaternion.Euler(rb.velocity.x * tilt, 90, rb.velocity.z * tilt);
     }
 
     
---
+++ Assets/Scripts/Propeller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Propeller : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.rotation = Quaternion.Euler(-90, rb.velocity.y + 10, rb.velocity.z + 10);
+    }
+}
