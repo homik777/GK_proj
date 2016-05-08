a384f20fe8853eaf9b831f60b1907181422603be
Rafał Potoczek <rafal.potoczek@gmail.com>
2016-05-08 20:36:58 +0200

    REPAIR

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public int speed; 
+	
+	void Update () {
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
