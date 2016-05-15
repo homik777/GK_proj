65bca448b966229c91f3d64c151045a12ddfab40
Rafał Potoczek <rafal.potoczek@gmail.com>
2016-05-15 16:22:06 +0200

    * Przeskalowanie - do poprawienia, średnio mi wyszło * Efekty cząsteczkowe - do poprawienia, żeby bardziej generyczne było

---
+++ Assets/Scripts/ToCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToCamera : MonoBehaviour
+{
+    Camera camera;
+    // Use this for initialization
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (camera != null)
+        {
+            transform.LookAt(camera.transform);
+            transform.forward *= -1;
+        }
+    }
+}
