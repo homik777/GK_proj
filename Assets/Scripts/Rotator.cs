e3ccbe96ae0f9daabd7ccfebf31488fce88cc68c
Rafał Potoczek <rafal.potoczek@gmail.com>
2016-06-04 20:58:20 +0200

    animation

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Rotator : MonoBehaviour {
+public class Rotator : MonoBehaviour
+{
 
-    public int speed; 
-	
-	void FixedUpdate () {
-        transform.eulerAngles += Vector3.forward * speed;
+    public int speed;
+    public Transform parent;
+    public Vector3 relativePosition;
+    private Quaternion relativeRotation;
+    float timer = 0f;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        transform.position = parent.position + relativePosition;
+        transform.rotation = parent.transform.rotation;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + speed * timer, transform.eulerAngles.z);
+        if (timer >= 360)
+        {
+            timer = 0f;
+        }
     }
 }
