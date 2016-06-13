406907f0060b9f07ae838a92ece522369870de01
homik777 <homik777@wp.pl>
2016-06-13 20:38:39 +0200

    *MainMenu scene *Added rotation script

---
+++ Assets/Scripts/ToParentFixer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToParentFixer : MonoBehaviour {
+
+
+    public Transform parent;
+    public bool hasRotator;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	   
+        transform.eulerAngles = parent.eulerAngles;
+        transform.position = parent.position;
+        
+	}
+}
